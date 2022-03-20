69895b0e2d08b7f0b12184608e42f88f225d38cc
Prashant Chauhan <chnpr2318@gmail.com>
2022-03-20 22:51:58 +0530

    everything is working fine

--- src/main/java/com/api/controller/MerchantController.java
+++ src/main/java/com/api/controller/MerchantController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
